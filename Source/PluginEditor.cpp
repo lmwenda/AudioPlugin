de689869e541faaa1b9986256ad5d9d6b65484a8
lmwenda <Lukemwen@hotmail.com>
2023-04-29 19:29:12 +0100

    Created Audio Parameters :hammer:

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("itzmaetyu's Audio Plugin", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
